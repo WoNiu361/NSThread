7b38b6c882d6c00c96c9dc1d970c0683af2820d9
lvyanhui <13733876213@163.com>
2018-11-18 18:21:31 +0800

    整理文件结构，添加图片

--- ThreadKnowledge/NSOperation/MyOperation.m
+++ ThreadKnowledge/NSOperation/MyOperation.m
@@ -35,4 +35,15 @@
     if (self.isCancelled) return;
 }
 
+/*
+ 自定义NSOperation
+ 
+ 自定义NSOperation的步骤很简单
+ 重写- (void)main方法，在里面实现想执行的任务
+ 
+ 重写- (void)main方法的注意点
+ 自己创建自动释放池（因为如果是异步操作，无法访问主线程的自动释放池）
+ 经常通过- (BOOL)isCancelled方法检测操作是否被取消，对取消做出响应
+ */
+
 @end
